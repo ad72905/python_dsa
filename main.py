35a86ebd6bab1349134430f98571e554d3161894
ad72905 <ashish.dalal.bitspilani@gmail.com>
2021-02-19 21:44:52 +0530

    driver program

---
+++ main.py
@@ -0,0 +1,25 @@
+from anagram import is_anagram
+from duplicate import find_duplicates
+from palindrome import is_palindrome, check_palindrome
+from reversedArray import reverse_array
+from theory import data_structures, abstract_data_types
+from integerReversion import reverse_integer, integer_reversion
+
+if __name__ == '__main__':
+    data_structures()
+    # abstract_data_types()
+    # reverse_array([12, 23, 31, 13, 21, 56, 35, 65])
+    # is_palindrome('madam')
+    # is_palindrome('monolithic')
+    # check_palindrome('madam')
+    # check_palindrome('monolithic')
+    # check_palindrome('radar')
+    # reverse_integer(1230)
+    # integer_reversion(1230)
+    # reverse_integer(81)
+    # integer_reversion(81)
+    # is_anagram('restful', 'fluster')
+    # is_anagram('cat', 'dog')
+    # is_anagram('cat', 'car')
+    # is_anagram('majestic', 'majesty')
+    # find_duplicates([2,5,4,2,3,5])
