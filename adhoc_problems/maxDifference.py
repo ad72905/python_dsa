6e713a1172fa395d9ace96c1c1eb24ac23a83737
ad72905 <ashish.dalal.bitspilani@gmail.com>
2021-03-02 00:37:52 +0530

    added documentation

--- adhoc_problems/maxDifference.py
+++ adhoc_problems/maxDifference.py
@@ -10,40 +10,55 @@
 
 # Running time complexity : O(N)
 # Space complexity : O(1)
-
 def max_profit_method_one(price):
-
+    # initializing the local minimum to be at index zero
     j = 0
+    # initializing profit to be zero (useful for empty array, one-element array)
     profit = 0
 
+    # iterating through stock prices, finding profits in each increasing sequence
+    # comparing through each such successive profit until we hit the end of array
     for i in range(1, len(price)):
 
-        if price[i-1] > price[i]:
+        # updating local minimum in case there is a decreasing sequence
+        if price[i - 1] > price[i]:
             j = i
 
-        if price[i-1] <= price[i] and \
-                (i + 1 == len(price) or price[i] >= price[i+1]):
-
+        # checking for potential profit
+        if price[i - 1] <= price[i] and \
+                (i + 1 == len(price) or price[i] >= price[i + 1]):
             print('Local Minima :', price[j])
             print('Local Maxima :', price[i])
             profit = max(profit, price[i] - price[j])
             print('Profit :', profit)
 
+    # return the max profit achieved
     return profit
 
+
+# Running time complexity : O(N)
+# Space complexity : O(1)
 def max_profit_method_two(price):
 
+    # return zero for an empty list of stock prices
+    if len(price) == 0:
+        return 0
+
+    # assuming the first element of array is the minimum
     min_stock_value = price[0]
+    # initializing the max profit to be zero
     max_profit = 0
 
+    # iterating through stock prices and updating max profit and min stock value
     for p in price:
         max_profit = max(max_profit, p - min_stock_value)
         min_stock_value = min(p, min_stock_value)
 
+    # return max profit thus achieved after completion of iteration
     return max_profit
 
 
+# driver program
 if __name__ == '__main__':
     print('Max Profit using 1st method :', max_profit_method_one([100, 180, 260, 310, 40, 535, 695]))
     print('Max Profit using 2nd method :', max_profit_method_two([100, 180, 260, 310, 40, 535, 695]))
-
